39d2e747d7aac945bc771f6ae5130de655e54677
Rahul Gupta <rahulguptaxyz15@gmail.com>
2020-01-30 17:16:29 +0530

    animated button on hover

---
+++ React/AnimatedButton/AnimatedButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AnimatedButton.scss';
+
+const animatedButton = () => {
+    return ( 
+        <div className='container'>
+          <div className='inner'>
+            <h3>Hey</h3>
+            <p>Note that the development build is not optimized.</p>
+          </div>
+        </div> 
+     );
+}
+
+export default animatedButton;
